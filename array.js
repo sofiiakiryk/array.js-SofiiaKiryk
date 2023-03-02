e5275e4e0300d551d68fd4ed4ef6fa552e3f1804
sofiiakiryk <107884647+sofiiakiryk@users.noreply.github.com>
2023-03-02 20:05:17 +0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,23 @@
+let numbers = [2, 5, 7, 3];
+console.log(numbers);
+let names = [
+  "Vitalii",
+  "Sofiia",
+  "Mariia",
+  "Jerry",
+  "Alex",
+  "Maksim",
+  "Lina",
+  "Merry",
+  "Mickle",
+  "Jordan",
+];
+console.log(names[0]);
+console.log(names[3]);
+console.log(names[7]);
+let arr = [];
+arr[0] = 10;
+arr[1] = 8;
+arr[4] = 100;
+
+console.log(arr);
